13ae8693cc9efd3ce7848cbbf3e7f457ac928282
logesh8220 <logeshthirumurugan@gmail.com>
2023-01-29 12:49:51 +0530

    login bug

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,10 +1,10 @@
 export const api = {
- codakta:"https://zenclassdashboard.onrender.com/api/codekata",
- webkata:"https://zenclassdashboard.onrender.com/api/webkata",
- completion:"https://zenclassdashboard.onrender.com/api/completion",
- tasks:"https://zenclassdashboard.onrender.com/api/tasks",
- total:"https://zenclassdashboard.onrender.com/api/todayandtotal",
- classes:"https://zenclassdashboard.onrender.com/api/classes",
- Login:"https://zenclassdashboard.onrender.com/api/login",
- Signin:"https://zenclassdashboard.onrender.com/api/signin",
+    codakta: "https://zenclassdashclone.onrender.com/api/codekata",
+    webkata: "https://zenclassdashclone.onrender.com/api/webkata",
+    completion: "https://zenclassdashclone.onrender.com/api/completion",
+    tasks: "https://zenclassdashclone.onrender.com/api/tasks",
+    total: "https://zenclassdashclone.onrender.com/api/todayandtotal",
+    classes: "https://zenclassdashclone.onrender.com/api/classes",
+    Login: "https://zenclassdashclone.onrender.com/api/login",
+    Signin: "https://zenclassdashclone.onrender.com/api/signin",
 }
